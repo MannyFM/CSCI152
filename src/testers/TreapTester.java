eeff3646be2a97e021f761b8d35c94019ebc071e
MannyFM <Alibek.manabayev@gmail.com>
2017-03-14 17:55:43 +0600

    TreapTester

---
+++ src/testers/TreapTester.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testers;
+
+//import ToolBox.util.PairTreap;
+//import ToolBox.util.TreapNode;
+//import ToolBox.util.TreapTree;
+
+/**
+ *
+ * @author manny
+ */
+public class TreapTester {
+
+  public static void main(String[] args) {
+	throw new UnsupportedOperationException("Not supported yet.");
+//	TreapTree <Integer> t = new TreapTree<>();
+//	for (int i = 0; i < 10; i++) {
+//	  int x = i;
+//	  t.insert(x);
+//	}
+//	t.Out();
+//	System.out.println(t.getSize() + ":" + t);
+//	System.out.println("<---------------------------->");
+//	
+//	TreapNode<Integer> node = t.getMinimum();
+//	
+//	t.Out();
+//	System.out.println("<---------------------------->");
+  }
+  
+}
