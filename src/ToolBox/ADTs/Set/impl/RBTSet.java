ed1cde68a9f2dba72bc3c43ed30dd1fc45bd45e9
MannyFM <Alibek.manabayev@gmail.com>
2017-03-28 13:52:46 +0600

    Red black not finished, just checkpoint

--- src/ToolBox/ADTs/Set/impl/RBTSet.java
+++ src/ToolBox/ADTs/Set/impl/RBTSet.java
@@ -15,7 +15,7 @@ import ToolBox.ADTs.Set.Set;
 public class RBTSet<T extends Comparable<? super T>> implements Set<T> {
 
   private static final boolean RED = true;
-  private static final boolean BLACK = true;
+  private static final boolean BLACK = false;
   private Node root;
   private Node nil;
   private int size;
@@ -24,12 +24,12 @@ public class RBTSet<T extends Comparable<? super T>> implements Set<T> {
 
 	private Node left, right, parent;
 	private boolean color;
-	private T value;
+	private final T value;
 
 	public Node(T value) {
-	  this.left = null;
-	  this.right = null;
-	  this.parent = null;
+	  this.left = nil;
+	  this.right = nil;
+	  this.parent = nil;
 	  this.value = value;
 	  this.color = RED;
 	}
@@ -45,11 +45,11 @@ public class RBTSet<T extends Comparable<? super T>> implements Set<T> {
 	@Override
 	public String toString() {
 	  String res = "";
-	  if (left != null) {
+	  if (left != nil) {
 		res += left + ", ";
 	  }
 	  res += value;
-	  if (right != null) {
+	  if (right != nil) {
 		res += ", " + right;
 	  }
 	  return res;
@@ -59,7 +59,11 @@ public class RBTSet<T extends Comparable<? super T>> implements Set<T> {
 
   public RBTSet() {
 	//Nil Node initialize
-	nil = new Node(null, nil, nil, nil, BLACK);
+	nil = new Node(null);
+	nil.left = nil;
+	nil.right = nil;
+	nil.parent = nil;
+	nil.color = BLACK;
 
 	root = nil;
 	size = 0;
@@ -67,32 +71,48 @@ public class RBTSet<T extends Comparable<? super T>> implements Set<T> {
 
   @Override
   public void add(T value) {
-	throw new UnsupportedOperationException("Not supported yet.");
+	if (this.contains(value)) {
+	  return;
+	}
+	size++;
+	Node newNode = new Node(value);
+	this.insert(newNode);
   }
 
   @Override
   public boolean contains(T value) {
-	throw new UnsupportedOperationException("Not supported yet.");
+	Node node = this.find(value);
+	return node != nil;
   }
 
   @Override
   public boolean remove(T value) {
-	throw new UnsupportedOperationException("Not supported yet.");
+	Node node = find(value);
+	if (node == nil)
+	  return false;
+	remove(node);
+	return true;
   }
 
   @Override
   public T removeAny() throws Exception {
-	throw new UnsupportedOperationException("Not supported yet.");
+	if (size == 0) {
+	  throw new Exception("Set is empty");
+	}
+	T result = root.value;
+	remove(result);
+	return result;
   }
 
   @Override
   public int getSize() {
-	throw new UnsupportedOperationException("Not supported yet.");
+	return this.size;
   }
 
   @Override
   public void clear() {
-	throw new UnsupportedOperationException("Not supported yet.");
+	root = nil;
+	size = 0;
   }
 
   @Override
@@ -103,20 +123,24 @@ public class RBTSet<T extends Comparable<? super T>> implements Set<T> {
 
   private String out(Node tree, int lvl) {
 	String res = "";
-	if (tree == null) {
+	if (tree == nil) {
 	  return res;
 	}
 	for (int i = 0; i < lvl; i++) {
 	  res += " ";
 	}
 	res += tree.value + "\n";
-	if (tree.left != null) {
+	if (tree.left != nil) {
 	  res += "l" + out(tree.left, lvl + 1);
 	}
-	if (tree.right != null) {
+	if (tree.right != nil) {
 	  res += "r" + out(tree.right, lvl + 1);
 	}
 	return res;
+  }
+  
+  public String out() {
+	return out(root, 0);
   }
 
   private void rotateLeft(Node tree) {
@@ -220,4 +244,103 @@ public class RBTSet<T extends Comparable<? super T>> implements Set<T> {
 	this.root.color = BLACK;
   }
 
+  private Node find(T value) {
+	Node node = this.root;
+	while (node != nil) {
+	  int compareResult = value.compareTo(node.value);
+	  if (compareResult == 0) {
+		return node;
+	  }
+	  if (compareResult < 0) {
+		node = node.left;
+	  } else {
+		node = node.right;
+	  }
+	}
+	return nil;
+  }
+
+  /**
+   * Negative value -> black height is bad non-negative -> OK
+   */
+  private int checkBlackHeight(Node node) {
+	if (node == nil) {
+	  return 1;
+	}
+	int left = checkBlackHeight(node.left);
+	int right = checkBlackHeight(node.right);
+	if (left < 0 || right < 0 || left != right) {
+	  return -1;
+	}
+	if (node.color == BLACK) {
+	  left++;
+	} else {
+	  if (node.parent.color == RED) {
+		return -1;
+	  }
+	}
+	return left;
+  }
+
+  public boolean isOk() {
+	return checkBlackHeight(root) >= 0;
+  }
+
+  private void transplant(Node tree, Node newNode) {
+	if (tree.parent == nil) {
+	  //tree is root
+	  root = newNode;
+	} else {
+	  if (tree.parent.left == tree) {
+		tree.parent.left = newNode;
+	  } else {
+		tree.parent.right = newNode;
+	  }
+	}
+	newNode.parent = tree.parent;
+  }
+
+  private void remove(Node tree) {
+	if (tree == nil) {
+	  return;
+	}
+	Node successor = tree;
+	Node doubleSuccessor = nil;
+	boolean successorColor = successor.color;
+	if (tree.left == nil) {
+	  doubleSuccessor = tree.right;
+	  transplant(tree, tree.right);
+	} else if (tree.right == nil) {
+	  doubleSuccessor = tree.left;
+	  transplant(tree, tree.left);
+	} else {
+	  successor = getMinimum(tree.right);
+	  successorColor = successor.color;
+	  doubleSuccessor = successor.right;
+	  if (successor.parent == tree) {
+		doubleSuccessor.parent = successor;
+	  } else {
+		transplant(successor, successor.right);
+		successor.right = tree.right;
+		successor.right.parent = successor;
+	  }
+	  transplant(tree, successor);
+	  successor.left = tree.left;
+	  successor.left.parent = tree;
+	  successor.color = tree.color;
+	}
+	if (successorColor == BLACK)
+	  deleteFix(doubleSuccessor);
+  }
+
+  private void deleteFix(Node tree) {
+	throw new UnsupportedOperationException("Not supported yet.");
+  }
+  
+  private Node getMinimum(Node tree) {
+	while (tree.left != nil) {
+	  tree = tree.left;
+	}
+	return tree;
+  }
 }
--- src/testers/RBTSetTester.java
+++ src/testers/RBTSetTester.java
@@ -13,15 +13,41 @@ import ToolBox.ADTs.Set.Set;
  * @author manny
  */
 public class RBTSetTester {
-  public static void main(String[] args) {
-	Set<Integer> set = new RBTSet<>();
-	for (int i = 0; i < 10; i++) {
-	  int val = i;
-	  if (i % 2 == 0)
-		val *= -1;
+
+  public static void main(String[] args) throws Exception {
+	RBTSet<Integer> set = new RBTSet<>();
+	//TODO: replace to SET<>
+
+//	set.add(1);
+//	System.out.println(1 + ":\n" + set);
+	int N = 10;
+	int maxVal = N * 2;
+	for (int i = 0; i < N; i++) {
+	  int val = (int) (Math.random() * maxVal);
+//	  int val = i;
+//	  if (i % 2 == 0)
+//		val *= -1;
 	  set.add(val);
-	  System.out.println(val + ":\n" + set);
-//	  System.out.println(set.out());
+	  System.out.println(set.isOk());
+//	  System.out.println(val + ":\n" + set);
 	}
+	System.out.println(set);
+
+	for (int i = 0; i <= maxVal; i++) {
+	  if (set.contains(i)) {
+		System.out.println(i + "+");
+	  }
+	}
+	System.out.println(set);
+	System.out.println(set.out());
+
+	set.removeAny();
+	System.out.println(set.isOk());
+//	for (int i = 0; i <= maxVal; i++) {
+//	  if (set.remove(i)) {
+//		System.out.println("removed " + i + " " + set);
+////		return;
+//	  }
+//	}
   }
 }
